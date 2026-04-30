49a8a707b08d8a142fc5a226a5d6adc5e5213866
MarioTerra_Dev <93275405+Mterra64@users.noreply.github.com>
2026-04-30 01:19:00 +0000

    original2

--- zoologico_v1/models/animal.py
+++ zoologico_v1/models/animal.py
@@ -1,12 +1,12 @@
 class Animal:
-def __init__(self, nome, especie, idade):
-self.nome = nome
-self.especie = especie
-self.idade = idade
+    def __init__(self, nome, especie, idade):
+        self.nome = nome
+        self.especie = especie
+        self.idade = idade
 
-def exibir_dados(self):
-# [span_1](start_span)Retorna a string básica de dados comum a todos os animais[span_1](end_span)
-return f"Nome: {self.nome} | Espécie: {self.especie} | Idade: {self.idade}"
+    def exibir_dados(self):
+        return f"Nome: {self.nome} | Espécie: {self.especie} | Idade: {self.idade}"
 
-def emitir_som(self):
-return "Som genérico de animal"
+    def emitir_som(self):
+        return "Som genérico de animal"
+        
